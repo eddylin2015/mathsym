41a35999fc42cf71daf55b65d3b0c5baf5391eda
eddylin2015 <eddylin2015@gmail.com>
2021-05-05 12:12:29 +0800

    a

--- website/esutils.py
+++ website/esutils.py
@@ -36,6 +36,35 @@ def Text2St(ans):
     ans = ans.replace(r"J(", r"sqrt(")
     return ans
 
+### input inequ_ans 
+
+def Text2Inequ(ans):
+    ans = re.sub(r",", r"|", ans)
+    ans = re.sub(r"[<][ ]*x[ ]*[<]", r"<x & x<", ans)
+    ans = re.sub(r"[>][ ]*x[ ]*[>]", r">x & x>", ans)
+    if ans.strip() == "": return "(-oo < x) & (x < oo)"
+    if ans.strip() == "": return "-3.1415926"
+    
+    ans = re.sub(r"X", r"x", ans)
+    ans = re.sub(r"(\d)x", r"\1*x", ans)
+    ans = ans = re.sub(r"[)][(]", r")*(", re.sub(r"(\d)x", r"\1*x", ans))
+    ans=re.sub(r"x[(]", r"x*(", ans)
+    ans = re.sub(r"x[ ]*", r"x", ans)
+    ans = ans.replace(r"x^", r"x**")
+
+    ans = re.sub(r"Y", r"y", ans)
+    ans = re.sub(r"(\d)y", r"\1*y", ans)
+    ans = ans = re.sub(r"[)][(]", r")*(", re.sub(r"(\d)y", r"\1*y", ans))
+    ans=re.sub(r"y[(]", r"y*(", ans)
+    ans = re.sub(r"y[ ]*", r"y", ans)
+    ans = ans.replace(r"y^", r"y**")
+    
+    ans=re.sub(r"(\d)[(]", r"\1*(", ans)
+    ans=re.sub(r"(\d)J[(]", r"\1*J(", ans)
+    ans = ans.replace(r"J(", r"sqrt(")
+    return ans
+
+
 def NTE2Table(data):
     return '<table><tr>{}</tr><tr>{}</tr></table>'.format(
            '<th>{}</th>'.format('</th><th>'.join( r for r in  r"編號,題型,題目,答案,作答,檢查,提示".split(","))),
