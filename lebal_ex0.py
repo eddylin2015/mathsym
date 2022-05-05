246cf1ae71c8609e9d5a90eb8622f408a56d8e3b
eddylin2015 <eddylin2015@gmail.com>
2022-05-05 10:17:43 +0800

    flask download mt_lebal.docx comment for developer

--- lebal_ex0.py
+++ lebal_ex0.py
@@ -1,71 +1,39 @@
-from flask import Flask, render_template, send_file
-from docx import Document
-from docx.shared import Inches
-from io import BytesIO
-import io
-import qrcode
-from docx.shared import Cm
-from docx.enum.section import WD_ORIENT #處理文件的直向/橫向
-from docx.shared import Pt
-from docx.table import _Cell
+"""
+資產標籤生成工具 下戴Wor檔案
+flask
+python-docx
+docx
+qrcoe
+"""
+from flask import Flask, render_template, send_file,request
+import io #from io import BytesIO
 import datetime 
 import copy 
+import qrcode
+from docx import Document
+from docx.shared import Inches,Cm,Pt
+from docx.enum.section import WD_ORIENT #處理文件的直向/橫向
+from docx.table import _Cell
+
 
 app = Flask(__name__)
-
-def add_qrcode(txt):
-    file = qrcode.make('sample text')
-    buf = io.BytesIO()
-    file.save(buf)
-    buf.seek(0)
-    #return send_file(buf, mimetype='image/jpeg')
-    return buf
-
-def set_cell_margins(cell: _Cell, **kwargs):
-    """
-    cell:  actual cell instance you want to modify
-
-    usage:
-
-        set_cell_margins(cell, top=50, start=50, bottom=50, end=50)
-
-    provided values are in twentieths of a point (1/1440 of an inch).
-    read more here: http://officeopenxml.com/WPtableCellMargins.php
-    """
-    tc = cell._tc
-    tcPr = tc.get_or_add_tcPr()
-    tcMar = OxmlElement('w:tcMar')
-
-    for m in [
-        "top",
-        "start",
-        "bottom",
-        "end",
-    ]:
-        if m in kwargs:
-            node = OxmlElement("w:{}".format(m))
-            node.set(qn('w:w'), str(kwargs.get(m)))
-            node.set(qn('w:type'), 'dxa')
-            tcMar.append(node)
-
-    tcPr.append(tcMar)
 
 
 @app.route('/')
 def index():
     t_=datetime.datetime.now().isoformat()
     return f"""
+    <a href="/showqrcode?q={t_}">img qrcode</a>
     <a href="/newdocx?r={t_}">new doc</a>
     <a href="/templdocx?r={t_}">templ doc</a>
     """
 
 @app.route('/templdocx')
 def templdocx_router():
-    document = Document("secret/lebal_templ_schsu951.docx")
+    document = Document("secret/lebal_templ_schsub951.docx")
     template = document.tables[0]
     tbl = template._tbl
     for i in range(10):
-        # Here we do the copy of the table
         new_tbl = copy.deepcopy(tbl)
         # Then we do the replacement
         # replaceText(document, '<>', 'New value')
@@ -75,8 +43,6 @@ def templdocx_router():
         paragraph._p.addnext(new_tbl)    
 
     for table in document.tables:
-        #table.columns[0].width = Cm(1.8)
-        #table.columns[1].width = Cm(3)
         hdr_cells = table.rows[0].cells
         file = qrcode.make('sample text')
         buf = io.BytesIO()
@@ -90,12 +56,20 @@ def templdocx_router():
         cell_run.font.name = '微軟正黑體'
         cell_run.font.size = Pt(8)
         cell_run.bold = True    
-    f = BytesIO()
+    f = io.BytesIO()
     document.save(f)
     length = f.tell()
     f.seek(0)
     return send_file(f, as_attachment=True, attachment_filename='report.doc')    
 
+@app.route('/showqrcode')
+def show_qrcode():
+    q = request.args.get('q')
+    file = qrcode.make(q)
+    buf = io.BytesIO()
+    file.save(buf)
+    buf.seek(0)
+    return send_file(buf, mimetype='image/jpeg')
 
 
 
@@ -108,57 +82,51 @@ def newdocx_router():
     section.top_margin=Cm(0.25)
     section.bottom_margin=Cm(0.1)    
     #section.orientation = WD_ORIENT.LANDSCAPE
-
     new_width, new_height = Cm(5), Cm(2.5)
     section.page_width = new_width
     section.page_height= new_height    
     for i in range(10):
         table = document.add_table(rows=1, cols=2)
         #table.allow_autofit = True
-        #table.rows[0].width=Cm(14)
         table.columns[0].width = Cm(1.8)
         table.columns[1].width = Cm(3)
-
         hdr_cells = table.rows[0].cells
-        #set_cell_margins(hdr_cells[0], top=0, start=0, bottom=0, end=0)
-        #set_cell_margins(hdr_cells[1], top=0, start=0, bottom=0, end=0)
         file = qrcode.make('sample text')
         buf = io.BytesIO()
         file.save(buf)
         buf.seek(0)
-        #p=document.add_picture(buf, width=Cm(1.85)) # width=Inches(1.25))
-        #hdr_cells[0].width = Cm(1.8)
         p=hdr_cells[0].paragraphs[0]
         run=p.add_run()
         run.add_picture(buf, width=Cm(1.8)) # width=Inches(1.25))
         #p.alignment=WD_ALIGN_PARAGRAPH.
-        #hdr_cells[1].add_paragraph( 'FDE-159(210916)\n學生實習使用的手提\n電腦\nMT512')
-        #hdr_cells[1].text= 'FDE-159(210916)\n學生實習使用的手提\n電腦\nMT512'
-        #hdr_cells[1].width = Cm(3.2)
         p=hdr_cells[1].paragraphs[0]
         cell_run=p.add_run('FDE-951(210916)\n學生實習使用的手提\n電腦\nMT512')
         cell_run.font.name = '微軟正黑體'
         cell_run.font.size = Pt(8)
         cell_run.bold = True
-
         #document.add_page_break()
-    #document.save('demo.docx')
-
-    f = BytesIO()
-    document.save(f)
+    f = io.BytesIO()
+    document.save(f) # or save('doc1.docx')
     length = f.tell()
     f.seek(0)
     return send_file(f, as_attachment=True, attachment_filename='report.doc')
 
 @app.route('/qrcode')
 def qrcode_router():
-    file = qrcode.make('sample text')
+    file = qrcode.make('sample text')     #type(file)  # qrcode.image.pil.PilImage
     buf = io.BytesIO()
-    file.save(buf)
+    file.save(buf)                        #img.save("some_file.png")  
     buf.seek(0)
     return send_file(buf, mimetype='image/jpeg')
 
 
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=84,debug=True)
+
+
+"""
+分析檔案結構工具
+"""
 
 from docx import Document
 from docx.document import Document as _Document
@@ -175,6 +143,12 @@ def iter_block_items(parent):
     Paragraph. *parent* would most commonly be a reference to a main
     Document object, but also works for a _Cell object, which itself can
     contain paragraphs and tables.
+
+    #document = Document('schsub951.docx')
+    #for block in iter_block_items(document):
+    #   print('found one')
+    #   print(block.text if isinstance(block, Paragraph) else '<table>')
+
     """
     if isinstance(parent, _Document):
         parent_elm = parent.element.body
@@ -190,65 +164,30 @@ def iter_block_items(parent):
         elif isinstance(child, CT_Tbl):
             yield Table(child, parent)
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=84,debug=True)
-    
-    #document = Document('schsub159.docx')
-    #for block in iter_block_items(document):
-    #   print('found one')
-    #   print(block.text if isinstance(block, Paragraph) else '<table>')
 
-    #img = qrcode.make('Some data here')
-    #type(img)  # qrcode.image.pil.PilImage
-    #img.save("some_file.png")    
+def set_cell_margins(cell: _Cell, **kwargs):
+    """
+    cell:  actual cell instance you want to modify
+    usage:
 
+        set_cell_margins(cell, top=50, start=50, bottom=50, end=50)
+
+    provided values are in twentieths of a point (1/1440 of an inch).
+    read more here: http://officeopenxml.com/WPtableCellMargins.php
     """
-    
-    document.add_heading('Document Title', 0)
-    
-    p = document.add_paragraph('A plain paragraph having some ')
-    p.add_run('bold').bold = True
-    p.add_run(' and some ')
-    p.add_run('italic.').italic = True
-    
-    document.add_heading('Heading, level 1', level=1)
-    document.add_paragraph('Intense quote', style='Intense Quote')
-    
-    document.add_paragraph(
-        'first item in unordered list', style='List Bullet'
-    )
-    document.add_paragraph(
-        'first item in ordered list', style='List Number'
-    )
-    
-    document.add_picture('some_file.png', width=Inches(1.25))
-    file = qrcode.make('sample text')
-    buf = io.BytesIO()
-    file.save(buf)
-    buf.seek(0)
-    document.add_picture(buf, width=Inches(1.25))
-    
-    records = (
-        (3, '101', 'Spam'),
-        (7, '422', 'Eggs'),
-        (4, '631', 'Spam, spam, eggs, and spam')
-    )
-    
-    table = document.add_table(rows=1, cols=3)
-    hdr_cells = table.rows[0].cells
-    hdr_cells[0].text = 'Qty'
-    hdr_cells[1].text = 'Id'
-    hdr_cells[2].text = 'Desc'
-    for qty, id, desc in records:
-        row_cells = table.add_row().cells
-        row_cells[0].text = str(qty)
-        row_cells[1].text = id
-        row_cells[2].text = desc
-    
-    document.add_page_break()
-    
-    document.save('demo.docx')
-    """
-    """
-    https://mlhive.com/2022/04/working-with-tables-in-python-docx
-    """
+    tc = cell._tc
+    tcPr = tc.get_or_add_tcPr()
+    tcMar = OxmlElement('w:tcMar')
+    for m in ["top","start","bottom","end"]:
+        if m in kwargs:
+            node = OxmlElement("w:{}".format(m))
+            node.set(qn('w:w'), str(kwargs.get(m)))
+            node.set(qn('w:type'), 'dxa')
+            tcMar.append(node)
+    tcPr.append(tcMar)
+
+
+"""
+https://python-docx.readthedocs.io/en/latest/
+https://mlhive.com/2022/04/working-with-tables-in-python-docx
+"""
